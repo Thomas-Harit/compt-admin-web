cadf9fab0d995ff44332364e22e8b5f1deb90c12
Thotho lasticot <thomas.harit@epitech.eu>
2025-12-07 17:52:33 +0100

    Updates

--- _app/immutable/entry/start.B1LYWzml.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/nulCiE75.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.D8adUEM5.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/aPswW3A9.js";export{o as load_css,r as start};
